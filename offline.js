0e4266456d6fe3afae388671ac6d09a5a3f3220f
KhoirulHS <wibuadalahrasterkuat@gmail.com>
2023-09-13 05:14:41 +0700

    rt

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1694555941,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/backgroundtile.png",
+		"images/player-sheet0.png",
+		"images/solidtile.png",
+		"images/jumpthrutile.png",
+		"images/covid-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
